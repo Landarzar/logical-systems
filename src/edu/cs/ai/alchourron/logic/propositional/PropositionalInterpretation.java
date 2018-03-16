3b58156acc815e0a23fe244e442478b7a3a2bb1f
Kai Sauerwald <kai.sauerwald@uni-dortmund.de>
2018-03-16 09:16:13 +0100

    Iterate over worlds

--- src/edu/cs/ai/alchourron/logic/propositional/PropositionalInterpretation.java
+++ src/edu/cs/ai/alchourron/logic/propositional/PropositionalInterpretation.java
@@ -1,27 +1,35 @@
 package edu.cs.ai.alchourron.logic.propositional;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 import edu.cs.ai.alchourron.logic.Interpretation;
 
 /***
  * Represents propositional interpretations.
+ * 
  * @author Kai Sauerwald
  *
- * @param <PSym> The type of symbols used in the signature
- * @param <S> The type of the signature
+ * @param <PSym>
+ *            The type of symbols used in the signature
+ * @param <S>
+ *            The type of the signature
  */
 public class PropositionalInterpretation<PSym, S extends PropositionalSignature<PSym>> implements Interpretation<S> {
 
 	S signature;
 	Set<PSym> trueValued;
-	
+
 	/***
 	 * Constructs a new interpretation
-	 * @param sig The signature over which the interpretation is defined
-	 * @param trueValued The elements which are considered to be true
+	 * 
+	 * @param sig
+	 *            The signature over which the interpretation is defined
+	 * @param trueValued
+	 *            The elements which are considered to be true
 	 */
 	public PropositionalInterpretation(S sig, PSym[] trueValued) {
 		this.trueValued = new HashSet<>();
@@ -30,36 +38,64 @@ public class PropositionalInterpretation<PSym, S extends PropositionalSignature<
 			this.trueValued.add(v);
 		}
 	}
-	
+
 	/***
 	 * Constructs a new interpretation
-	 * @param sig The signature over which the interpretation is defined
-	 * @param trueValued The elements which are considered to be true
+	 * 
+	 * @param sig
+	 *            The signature over which the interpretation is defined
+	 * @param trueValued
+	 *            The elements which are considered to be true
 	 */
 	public PropositionalInterpretation(S sig, Collection<PSym> trueValued) {
 		this.trueValued = new HashSet<>(trueValued);
 	}
-	
+
 	/***
 	 * Checks where under this interpretation {@link symbol} is true.
+	 * 
 	 * @author Kai Sauerwald
-	 * @param symbol The symbol
+	 * @param symbol
+	 *            The symbol
 	 */
 	public boolean isTrue(PSym symbol) {
 		return trueValued.contains(symbol);
 	}
-	
+
+	/***
+	 * Gets all variables which are true in this world.
+	 * 
+	 * @author Kai Sauerwald
+	 */
+	public Set<PSym> getAllTrue() {
+		return Collections.unmodifiableSet(trueValued);
+	}
+
 	/***
 	 * Checks where under this interpretation {@link symbol} is false.
+	 * 
 	 * @author Kai Sauerwald
-	 * @param symbol The symbol
+	 * @param symbol
+	 *            The symbol
 	 */
 	public boolean isFalse(PSym symbol) {
 		return !isTrue(symbol);
 	}
 
+	/***
+	 * Gets all variables which are false in this world.
+	 * 
+	 * @author Kai Sauerwald
+	 */
+	public Set<PSym> getAllFalse() {
+		HashSet<PSym> set = new HashSet<>(signature.getSymbols());
+		set.removeAll(trueValued);
+		return Collections.unmodifiableSet(set);
+	}
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see cs.ai.logic.Interpretation#getSignature()
 	 */
 	@Override
--- src/edu/cs/ai/alchourron/logic/propositional/PropositionalSignature.java
+++ src/edu/cs/ai/alchourron/logic/propositional/PropositionalSignature.java
@@ -1,53 +1,128 @@
 package edu.cs.ai.alchourron.logic.propositional;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import edu.cs.ai.alchourron.logic.Signature;
 
 /***
- * Represents a propositional signature, which uses Elements of the type Psym as symbol supply.
+ * Represents a propositional signature, which uses Elements of the type Psym as
+ * symbol supply.
+ * 
  * @author Kai Sauerwald
  *
- * @param <PSym> The type of the symbols
+ * @param <PSym>
+ *            The type of the symbols
  */
-public class PropositionalSignature<PSym> implements Signature {
-	
+public class PropositionalSignature<PSym> implements Signature, Iterable<PropositionalInterpretation<PSym, PropositionalSignature<PSym>>> {
+
 	/***
 	 * The symbols of this signature
+	 * 
 	 * @author Kai Sauerwald
 	 */
-	HashSet<PSym> symbols;
-	
+	ArrayList<PSym> symbols;
+
 	/***
 	 * Constructs a new propositional signature
+	 * 
 	 * @author Kai Sauerwald
-	 * @param symbols The symbols of this signature
+	 * @param symbols
+	 *            The symbols of this signature
 	 */
 	public PropositionalSignature(PSym[] symbols) {
-		this.symbols = new HashSet<>();
+		this.symbols = new ArrayList<>();
 		for (int i = 0; i < symbols.length; i++) {
 			PSym v = symbols[i];
 			this.symbols.add(v);
 		}
 	}
-	
+
 	/***
 	 * Constructs a new propositional signature
+	 * 
 	 * @author Kai Sauerwald
-	 * @param symbols The symbols of this signature
+	 * @param symbols
+	 *            The symbols of this signature
 	 */
 	public PropositionalSignature(Collection<PSym> symbols) {
-		this.symbols = new HashSet<>(symbols);
+		this.symbols = new ArrayList<>(symbols);
 	}
-	
+
 	/***
-	 * Returns the set of the symbols of this signature 
+	 * Returns the set of the symbols of this signature
+	 * 
 	 * @author Kai Sauerwald
 	 */
-	public Set<PSym> getSymbols(){
-		return Collections.unmodifiableSet(symbols);
+	public List<PSym> getSymbols() {
+		return Collections.unmodifiableList(symbols);
+	}
+
+    /**
+     * Returns a {@code Stream} of all interpretations under this signature
+     * @author Kai Sauerwald.
+     */
+	public Stream<PropositionalInterpretation<PSym, PropositionalSignature<PSym>>> stream() {
+		return StreamSupport.stream(Spliterators.spliterator(iterator(), /* initial size */ 0L, Spliterator.IMMUTABLE), false);
+	}
+
+	/**
+	 * Creates an iterator, that iterates over all interpretations under this signature.
+	 * @author Kai Sauerwald
+	 * @see java.lang.Iterable#iterator()
+	 */
+	@Override
+	public Iterator<PropositionalInterpretation<PSym, PropositionalSignature<PSym>>> iterator() {
+		String cmp = "";
+		for (int i = 0; i < symbols.size(); i++) {
+			cmp += "1";
+		}
+		final String cmp2 = cmp;
+
+		PropositionalSignature<PSym> sig = this;
+		return new Iterator<PropositionalInterpretation<PSym, PropositionalSignature<PSym>>>() {
+
+			BigInteger bigint = BigInteger.ZERO;
+			String max = cmp2;
+
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see java.util.Iterator#hasNext()
+			 */
+			@Override
+			public boolean hasNext() {
+				return bigint.compareTo(new BigInteger(max, 2)) <= 0;
+			}
+
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see java.util.Iterator#next()
+			 */
+			@Override
+			public PropositionalInterpretation<PSym, PropositionalSignature<PSym>> next() {
+				LinkedList<PSym> temp = new LinkedList<>();
+				for (int i = 0; i < symbols.size(); i++) {
+					if (bigint.testBit(i))
+						temp.add(symbols.get(i));
+				}
+
+				bigint = bigint.add(BigInteger.ONE);
+				return new PropositionalInterpretation<PSym, PropositionalSignature<PSym>>(sig, temp);
+			}
+
+		};
 	}
 }
