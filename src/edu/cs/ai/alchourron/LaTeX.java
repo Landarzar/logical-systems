a3f93065969d5e823fcc7288d0293eb64bfec390
Kai Sauerwald <kai.sauerwald@fernuni-hagen.de>
2018-10-22 10:57:31 +0200

    Exception for the case of not implementing LaTeX

--- src/edu/cs/ai/alchourron/LaTeX.java
+++ src/edu/cs/ai/alchourron/LaTeX.java
@@ -3,6 +3,7 @@
  */
 package edu.cs.ai.alchourron;
 
+
 /**
  * Marks objects which can be convertet to a LaTeX representation
  * @author Kai Sauerwald
@@ -13,6 +14,9 @@ public interface LaTeX {
 	/**
 	 * Builds a LaTeX representation of this object.
 	 * @author Kai Sauerwald
+	 * @throws UnsupportedOperationException When not implemented.
 	 */
-	public String toLaTeX();
+	public default String toLaTeX() {
+		throw new UnsupportedOperationException();
+	}
 }
