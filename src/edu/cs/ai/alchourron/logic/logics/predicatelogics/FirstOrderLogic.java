872a8596e1f7b52f2fd6a762bbd68c3e1d83f8b6
Kai Sauerwald <kai.sauerwald@udo.edu>
2021-03-03 15:55:15 +0100

    Make "Generalised" Logic classes abstract

--- src/edu/cs/ai/alchourron/logic/logics/predicatelogics/FirstOrderLogic.java
+++ src/edu/cs/ai/alchourron/logic/logics/predicatelogics/FirstOrderLogic.java
@@ -3,9 +3,13 @@
  */
 package edu.cs.ai.alchourron.logic.logics.predicatelogics;
 
+import java.util.function.Function;
+
 import edu.cs.ai.alchourron.logic.Formula;
 import edu.cs.ai.alchourron.logic.Interpretation;
+import edu.cs.ai.alchourron.logic.semantics.interpretations.FiniteStructure;
 import edu.cs.ai.alchourron.logic.syntax.formula.ClassicalQuantifier;
+import edu.cs.ai.alchourron.logic.syntax.formula.FormulaQuantification;
 
 /***
  * Logical System representing first order logic.
@@ -17,6 +21,38 @@ import edu.cs.ai.alchourron.logic.syntax.formula.ClassicalQuantifier;
  * @param <V> The type for Variables
  */
 public class FirstOrderLogic<R, K, V> extends GeneralizedFirstOrderLogic<R, K, V, ClassicalQuantifier, FOSignature<R, K, V>> {
+
+	@Override
+	protected <U> Boolean evalQuantifiedFormua(FiniteStructure<U, R, K, FOSignature<R, K, V>> interpretation,
+			FormulaQuantification<ClassicalQuantifier, V, FOSignature<R, K, V>> formula, Function<V, U> valuation) {
+
+		if (formula.getQuantifyer() == ClassicalQuantifier.FORALL) {
+			return interpretation.getUniverse().stream().allMatch(u -> {
+
+				Function<V, U> nval = var -> {
+					if (var.equals(formula.getVariables()))
+						return u;
+					return valuation.apply(var);
+				};
+
+				return eval(interpretation, formula.getQuantified(), nval);
+			});
+		}
+		else if (formula.getQuantifyer() == ClassicalQuantifier.EXISTS) {
+			return interpretation.getUniverse().stream().anyMatch(u -> {
+
+				Function<V, U> nval = var -> {
+					if (var.equals(formula.getVariables()))
+						return u;
+					return valuation.apply(var);
+				};
+
+				return eval(interpretation, formula.getQuantified(), nval);
+			});
+		}
+		
+		throw new UnsupportedOperationException("Unknown quantifier" + formula.getQuantifyer());
+	}
 
 
 
